b80135ee639abc0b67f36ee02ba1df8dbb363071
amigdol1 <alex.migdol@gmail.com>
2016-07-13 23:46:31 -0700

    add html and js logic for collecting input for pizza with radio buttons and checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-function pizza {
-  this.toppings: [],
-  this.size: size
-}
+// function pizza {
+//   this.toppings: [],
+//   this.size: size
+// }
+
+$(document).ready(function() {
+  $("form#pizza").submit(function(event) {
+    event.preventDefault();
+    var size = $("input:radio[name=size]:checked").val();
+    var toppings = $("input:checkbox:checked").map(function(){
+        return $(this).val();
+    }).toArray()
+    console.log(size)
+    console.log(toppings)
+  });
+});
