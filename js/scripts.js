616c8e812cc8dc722cf4b45b4fc158197f551e9e
amigdol1 <alex.migdol@gmail.com>
2016-07-13 23:50:44 -0700

    fix a bug with the pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-// function pizza {
-//   this.toppings: [],
-//   this.size: size
-// }
+function pizza(toppings, size) {
+  this.toppings = [],
+  this.size = size
+}
 
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
-    var size = $("input:radio[name=size]:checked").val();
     var toppings = $("input:checkbox:checked").map(function(){
         return $(this).val();
     }).toArray()
+    var size = $("input:radio[name=size]:checked").val();
     console.log(size)
     console.log(toppings)
   });
