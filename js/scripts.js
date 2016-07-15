622ff44e1491da820c8a48c36cc48c6937f1d618
amigdol1 <alex.migdol@gmail.com>
2016-07-15 12:36:03 -0700

    clean up some code

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,15 @@ $(document).ready(function() {
   var pizzaInput = new Pizza();
   $("form#pizza").submit(function(event) {
     event.preventDefault();
-
-    var name = $("#name").val();
-    var phone = $("#phone").val();
+    var inputName = $("#name").val();
+    var inputPhone = $("#phone").val();
     var inputToppings = $("input:checkbox:checked").map(function(){
       return $(this).val();
-    }).toArray()
+      }).toArray()
     var inputSize = $("input:radio[name=size]:checked").val();
-
     var newPizza = new Pizza(inputToppings, inputSize);
     newPizza.cost();
 
-    $("#result").text("Thank you, " + name + ". You ordered a fancy pizza with the following item(s): " + newPizza.toppings.join(", ") + ". This will be ready in 2 to 4 hours and will cost: $" + newPizza.cost());
+    $("#result").text("Thank you, " + inputName + ". You ordered a fancy pizza with the following item(s): " + newPizza.toppings.join(", ") + ". This will be ready in 2 to 4 hours and will cost: $" + newPizza.cost());
   });
 });
